aec46496449c3a98cda07b28e4adfe246ebb6f2d
SMH <shannonmhenry1@gmail.com>
2019-04-08 16:43:27 -0500

    fixed title

--- public/app.js
+++ public/app.js
@@ -1,10 +1,10 @@
 $.getJSON("/articles", function(data) {
   for (var i = 0; i < data.length; i++) {
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].summary + "<br />" + data[i].link + "</p>");
+    $("#articles").append("<p data-id='" + data[i]._id + "'>" + "<h3>" + data[i].title + "</h3>" + "<br />" + data[i].summary + "<br />" + data[i].link + "</p>");
   }
 });
 
-$(document).on("click", "p", function() {
+$(document).on("click", "h3", function() {
   $("#notes").empty();
   var noteId = $(this).attr("data-id");
 
